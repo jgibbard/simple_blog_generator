e43b380cb586b7e0ab154abeb5227e7a8c034024
James Gibbard <james@gibbard.me>
2020-12-29 21:15:58 +0000

    Add pagination to home page

--- simple_blog_generator/simple_blog_generator.py
+++ simple_blog_generator/simple_blog_generator.py
@@ -74,9 +74,10 @@ class SimpleBlogGenerator():
         # Generate the category pages
         self._generate_category_pages()
         # Generate the home page
-        self._generate_home_page()
+        self._generate_home_pages()
 
     def get_most_recent_post_titles(self, category, number, offset=0):
+        # Get the N most recent posts titles in a specified category
         # Validate inputs
         assert number != 0
         assert category in self.categories or category == "all_posts"
@@ -88,15 +89,28 @@ class SimpleBlogGenerator():
         number_of_posts = len(post_titles)
         # Catch case where there are no posts in the category
         if number_of_posts == 0:
-            return None
+            return list()
         # Make sure the offset doesn't exceed the size of the list
         assert offset < number_of_posts
         # Return a list of the most recent posts
         if number_of_posts >= (number + offset):
             return post_titles[offset:offset+number]
         else:
-            # If there is not as many posts are requested, then teturn as many as possible
+            # If there is not as many posts are requested, then return as many as possible
             return post_titles[offset:]
+
+    def get_most_recent_posts(self, category, number, offset=0):
+        # Get the N most recent posts in a specified category
+        # Validate inputs
+        assert number != 0
+        assert category in self.categories or category == "all_posts"
+        # If the posts haven't been sorted yet, then sort them
+        if not self.sorted_posts:
+           self._sort_posts()
+        # Get titles of the most recent posts in a given category
+        post_titles = self.get_most_recent_post_titles(category, number, offset)
+        # Construct dictionary of the most recent posts
+        return [self.posts[post_title]["post"] for post_title in post_titles]
 
     def clean(self):        
         # Clear post cache
@@ -177,6 +191,8 @@ class SimpleBlogGenerator():
                     self.posts[post_title]["post"]["main_image"] = meta["main_image"][0]
 
     def _sort_posts(self):
+        # Create a dictionary of lists where the posts are split into categories and sorted by
+        # post date.
         if not self.posts:
             self._get_posts()
         self.sorted_posts = {category : list() for category in self.categories}
@@ -188,22 +204,23 @@ class SimpleBlogGenerator():
             date = datetime.datetime.strptime(date_string, self.date_format)
             self.sorted_posts[post_details["category"]].append((post_title, date))
             self.sorted_posts["all_posts"].append((post_title, date))
-
         # Remove all categories with no posts
         self.categories = [
             category for category in self.categories if len(self.sorted_posts[category])]
         self.sorted_posts = {
             category:self.sorted_posts[category] for category in self.sorted_posts.keys() \
                 if len(self.sorted_posts[category])}
-
         # Sort each category in date order with newest posts having lower indices
         for category in self.categories:
             self.sorted_posts[category] = sorted(
                 self.sorted_posts[category], key=lambda elem : elem[1], reverse=True)
+        # Also create an extra category of all posts and sort them all in date order.
+        # This is used for the home pages.
         self.sorted_posts["all_posts"] = sorted(
                 self.sorted_posts["all_posts"], key=lambda elem : elem[1], reverse=True)
 
     def _read_markdown(self, filename):
+        # Open a markdown file and convert it to HTML
         with open(filename, "r") as f:
             md = markdown.Markdown(extensions=["meta","fenced_code", "codehilite"])
             html = md.convert(f.read())
@@ -211,6 +228,7 @@ class SimpleBlogGenerator():
             return meta, html
 
     def _get_post_file_paths(self, search_directory):
+        # Locate all markdown files in the content directory
         posts = []
         for root, _, filenames in os.walk(search_directory):
             for filename in filenames:
@@ -218,24 +236,53 @@ class SimpleBlogGenerator():
                     posts.append(os.path.join(root, filename))
         return posts
 
-    def _copy_static_assets(self):        
+    def _copy_static_assets(self):
+        # Copy the global static assets (logos, CSS files, javascript etc.) over to the output
+        # directory.
         if os.path.isdir(self.static_directory):
             static_assets_output_directory = os.path.join(self.output_directory, "static")
             shutil.copytree(self.static_directory, static_assets_output_directory)
 
     def _copy_post_assets(self):
+        # Copy all the post assets (typically images, CSS files and javascript files) over to the
+        # output directory.
         assert self.posts is not None
         for post_title, post_details in self.posts.items():
-            # If there is an assets folder for the post
+            # If there is an assets folder for the post then copy everything over to output dir
             if post_details["assets"]:
                 assets_output_directory = os.path.join(self.output_directory, post_title)
                 shutil.copytree(post_details["assets"],
                                 assets_output_directory,
                                 ignore=shutil.ignore_patterns("*.md"))
+            # Otherwise just make the post directory
             else:
                 os.mkdir(os.path.join(self.output_directory, post_title), self.output_permissions)
 
+    def _get_page_name(self, page_number, number_of_pages):
+        # Get the current, next, and previous page names
+        # Used when setting the back and forward links on paginated pages
+        # Set the page name
+        if page_number == 0:
+            page_name = "index.html"
+        else:
+            page_name = f"page{page_number}.html"
+        # Set the page name for the previous page link
+        if page_number == 0:
+            previous_page = None
+        elif page_number == 1:
+            previous_page = "index.html"
+        else:
+            previous_page = f"page{page_number-1}.html"
+        # Set the page name for the next page link
+        if page_number == (number_of_pages - 1):
+            next_page = None
+        else:
+            next_page = f"page{page_number+1}.html"
+        # Return page link names
+        return page_name, previous_page, next_page
+
     def _generate_post_pages(self):
+        # Generate all the post pages
         assert self.posts is not None
         post_template = self.template_env.get_template("post.html")
         for post_title, post_details in self.posts.items():
@@ -252,16 +299,14 @@ class SimpleBlogGenerator():
                 )
 
     def _generate_category_page(self, category, page_name, previous_page, next_page, offset):
+        # Generate a single category page
         assert self.posts is not None
         assert self.sorted_posts is not None
-        page_posts_titles = self.get_most_recent_post_titles(
+        # Get the posts that will appear on this page
+        posts_on_this_page = self.get_most_recent_posts(
             category, self.category_page_post_limit, offset)
-
-        posts_on_this_page = []
+        # Generate each category page
         category_template = self.template_env.get_template("category.html")
-        for post_title in page_posts_titles:
-            posts_on_this_page.append(self.posts[post_title]["post"])
-
         output_file = os.path.join(self.output_directory, category.lower(), page_name)
         self._write_html_file(output_file,
             category_template.render(
@@ -280,15 +325,15 @@ class SimpleBlogGenerator():
             )
 
     def _generate_category_pages(self):
+        # Generate all category pages
         assert self.posts is not None
         assert self.sorted_posts is not None
-
         for category in self.categories:
             # Create category directory
             category_directory = os.path.join(self.output_directory, category.lower())
             if not os.path.isdir(category_directory):
                 os.mkdir(category_directory, self.output_permissions)
-
+            # Get total number of posts in the category
             number_of_posts = len(self.sorted_posts[category])
             # Get the number of pages required to show all the posts within the category
             number_of_pages = number_of_posts // self.category_page_post_limit
@@ -296,57 +341,53 @@ class SimpleBlogGenerator():
                 number_of_pages += 1
             # Generate each category page
             for page_number in range(number_of_pages):
-                # Set the page name
-                if page_number == 0:
-                    page_name = "index.html"
-                else:
-                    page_name = f"page{page_number}.html"
-                # Set the page name for the previous and next page links
-                if page_number == 0:
-                    previous_page = None
-                elif page_number == 1:
-                    previous_page = "index.html"
-                else:
-                    previous_page = f"page{page_number-1}.html"
-                
-                if page_number == (number_of_pages - 1):
-                    next_page = None
-                else:
-                    next_page = f"page{page_number+1}.html"
-
+                # Set the page name for the current, previous, and next page links
+                page_name, previous_page, next_page = self._get_page_name(page_number,
+                                                                          number_of_pages)
                 # Offset in the list of posts to include on a specific page
                 offset = page_number * self.category_page_post_limit
                 # Generate a category page
                 self._generate_category_page(category, page_name, previous_page, next_page, offset)
 
-    def _generate_home_page(self):
+    def _generate_home_pages(self):
+        # Generate all home pages
         assert self.posts is not None
         assert self.sorted_posts is not None      
-
-        home_posts_titles = self.get_most_recent_post_titles(
-            "all_posts", self.index_page_post_limit, 0)
-
-        home_posts = []
-        for post_title in home_posts_titles:
-            home_posts.append(self.posts[post_title]["post"])
-
-        home_template = self.template_env.get_template("home.html")
-
-        output_file = os.path.join(self.output_directory, "index.html")
-        self._write_html_file(output_file,
-            home_template.render(
-                copyright=self.copyright,
-                website_name=self.website_name,
-                base_url="",
-                categories=self.categories,
-                page_category="Home",
-                author=self.default_author,
-                title=self.website_name,
-                description=self.website_description,
-                posts=home_posts
+        # Get total number of posts
+        number_of_posts = len(self.sorted_posts["all_posts"])
+        # Get the number of pages required to show all the posts
+        number_of_pages = number_of_posts // self.index_page_post_limit
+        if number_of_posts % self.index_page_post_limit:
+            number_of_pages += 1
+        # Generate each index page
+        for page_number in range(number_of_pages):
+            # Set the page name for the current, previous, and next page links
+            page_name, previous_page, next_page = self._get_page_name(page_number, number_of_pages)
+            # Offset in the list of posts to include on a specific page
+            offset = page_number * self.index_page_post_limit
+            # Get the posts that will appear on this page
+            posts_on_this_page = self.get_most_recent_posts(
+                "all_posts", self.index_page_post_limit, offset)
+            # Generate an index page
+            home_template = self.template_env.get_template("home.html")
+            output_file = os.path.join(self.output_directory, page_name)
+            self._write_html_file(output_file,
+                home_template.render(
+                    copyright=self.copyright,
+                    website_name=self.website_name,
+                    base_url="",
+                    categories=self.categories,
+                    page_category="Home",
+                    author=self.default_author,
+                    title=self.website_name,
+                    description=self.website_description,
+                    previous_page=previous_page,
+                    next_page=next_page,
+                    posts=posts_on_this_page
+                    )
                 )
-            )
 
     def _write_html_file(self, file_name, html_string):
+        # Write the HTML files to the output directory 
         with open(file_name, "w") as f:
             f.write(html_string)
